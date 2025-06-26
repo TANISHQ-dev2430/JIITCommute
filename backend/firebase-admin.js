8475ff315e8567d9d55218185326dfb1d46672cf
Tanishq Kumar <ptanishq240904@gmail.com>
2025-06-26 20:08:47 +0530

    Update firebase-admin.js

--- backend/firebase-admin.js
+++ backend/firebase-admin.js
@@ -1,12 +1,10 @@
 const admin = require('firebase-admin');
 
-let serviceAccount;
+// Parse the JSON from env variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
-if (process.env.FIREBASE_ADMIN_KEY) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
-} else {
-  serviceAccount = require('./serviceAccountKey.json'); 
-}
+// Fix the private_key: convert \\n to actual \n
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
