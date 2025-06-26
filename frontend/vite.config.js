db5b3905453ec350f3ce288de07b65f92711c05c
Tanishq Kumar <ptanishq240904@gmail.com>
2025-06-26 21:23:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-       '/users': 'http://192.168.1.2:4000',
-      '/trips': 'http://192.168.1.2:4000',
-    },
-  },
+  plugins: [react(), tailwindcss()]
 })
